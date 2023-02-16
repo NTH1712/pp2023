af285e7cd765e1b369d9a0a1adff78f845537c29
NTH1712 <hoangnt.bi12-171@st.usth.edu.vn>
2023-02-16 16:27:04 +0700

    First commit for Lab1

---
+++ 1.student.mark.py
@@ -0,0 +1,29 @@
+S = dict()
+n = int(input("How many students you want to add: "))
+for i in range (0,n):
+    x = input("Student name: ")
+    y = input("Student id: ") 
+    z = input ("Student date of birth: ")
+    S[x] = (y,z)
+print(S)
+
+D = dict()
+m = int(input("How many courses are there: "))
+for i in range (0,m):
+    a = input ("Course id: ")
+    b = input ("Course name: ")
+    D[a, b] = (S)
+print(D)    
+
+M = dict()
+l = int(input("Number of student "))    
+for i in range (0,l):
+    my_tuple = [a, y]
+    my_tuple = my_tuple + (l)
+print(my_tuple)
+
+
+
+
+ 
+
